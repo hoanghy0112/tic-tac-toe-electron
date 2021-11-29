5ec0e1054ba1a8655ab516ddc86463d2d303a85f
Hoanghy_windows10 <hoanghy01122003@gmail.com>
2021-11-30 04:03:42 +0700

    Change about-us page

--- src/renderer/pages/AboutUsPage/index.js
+++ src/renderer/pages/AboutUsPage/index.js
@@ -5,7 +5,11 @@ import PageScaffold from 'renderer/components/PageScaffold';
 const AboutUsPage = () => {
 	return (
 		<PageScaffold background={} previousPage="/">
-			<div>Nothing here <br />See you soon in next version</div>
+			<div className="">
+				<div>Nothing here <br />See you soon in next version</div>
+				<br />
+				<div className="">My github: <a href="https://github.com/hoanghy0112">https://github.com/hoanghy0112</a></div>
+			</div>
 		</PageScaffold>
 	);
 };
